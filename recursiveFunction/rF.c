4580d2610be412ecd27c1f4b36afc13d07779e98
ishikasofat <43296207+ishikasofat@users.noreply.github.com>
2020-10-01 15:51:25 +0530

    Update rF.c

--- recursiveFunction/rF.c
+++ recursiveFunction/rF.c
@@ -5,6 +5,11 @@ int factorial(int number)
 	return (number <= 1) ? number : number * factorial(number - 1);
 }
 
+int factorial_new(int number)
+{
+	return (number <= 1) ? number : number * factorial(number - 1);
+}
+
 int main(int argc, char *argv[])
 {
 	printf("%d", factorial( atoi(argv[1]) ));
